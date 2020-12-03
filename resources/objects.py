1ba7b9e7ac68f77b6512be51ea157998e406bb2b
jasonflorentino <jasonmflorentino@gmail.com>
2020-12-03 12:06:59 -0500

    🔑 Refactored game objects classes to init with hash table

--- resources/objects.py
+++ resources/objects.py
@@ -6,41 +6,34 @@ OBJECT CLASSES
 """
 
 class GameObject():
-    def __init__(self, name, description, odour, isVisible):
-        self.name = name
-        self.description = description
-        self.odour = odour
-        self.isVisible = isVisible
+    def __init__(self, arguments):
+        self.name = arguments.get("name", False)
+        self.description = arguments.get("description", False)
+        self.odour = arguments.get("odour", False)
+        self.isVisible = arguments.get("isVisible", False)
     
     def toggleVisibility(self):
         self.isVisible = not self.isVisible
         return self
 
 class Item(GameObject):
-    def __init__(self, name, description, odour, isVisible,
-            isTakeable, isUseable, isReusable, isUsed,
-            takeResp, useResp, dropResp):
-       GameObject.__init__(self, name, description, odour, isVisible)
-       self.isTakeable = isTakeable
-       self.isUseable = isUseable
-       self.isReusable = isReusable
-       self.isUsed = isUsed
-       self.takeResp = takeResp
-       self.useResp = useResp
-       self.dropResp = dropResp
+    def __init__(self, arguments):
+       GameObject.__init__(self, arguments)
+       self.isTakeable = arguments.get("isTakeable", False)
+       self.isUseable = arguments.get("isUseable", False)
+       self.isReusable = arguments.get("isReusable", False)
+       self.isUsed = arguments.get("isUsed", False)
+       self.takeResp = arguments.get("takeResp", False)
+       self.useResp = arguments.get("useResp", False)
+       self.dropResp = arguments.get("dropResp", False)
 
 class Container(Item):
-    def __init__(self, name, description, odour, isVisible,
-        isTakeable, isUseable, isReusable, isUsed,
-        takeResp, useResp, dropResp, isContainer, isOpen,
-        isCloseable, contents):
-        Item.__init__(self, name, description, odour, isVisible,
-            isTakeable, isUseable, isReusable, isUsed,
-            takeResp, useResp, dropResp)
-        self.isContainer = isContainer
-        self.isOpen = isOpen
-        self.isCloseable = isCloseable
-        self.contents = contents
+    def __init__(self, arguments):
+        Item.__init__(self, arguments)
+        self.isContainer = arguments.get("isContainer", True)
+        self.isOpen = arguments.get("isOpen", False)
+        self.isCloseable = arguments.get("isCloseable", True)
+        self.contents = arguments.get("contents", [])
     
     def addToContents(self, objectString):
         self.contents.append(objectString)
@@ -71,17 +64,27 @@ class Container(Item):
             return self
 
 class Npc(GameObject):
-    def __init__(self, name, description, odour, isVisible, isTalkable, talkResp):
-        GameObject.__init__(self, name, description, odour, isVisible)
-        self.isTalkable = isTalkable
-        self.talkResp = talkResp
+    def __init__(self, arguments):
+        GameObject.__init__(self, arguments)
+        self.isTalkable = arguments.get("isTalkable", False)
+        self.talkResp = arguments.get("talkResp", False)
 
 """
 GENERATE OBJECTS
 """
 
 def createObjects():
-    gameObjects = {**itemObjectData, **containerObjectData, **npcObjectData}
+    gameObjects = {}
+    # Create items
+    for k, v in itemObjectData.items():
+        gameObjects[k] = Item(v)
+    # Create containers
+    for k, v in containerObjectData.items():
+        gameObjects[k] = Container(v)
+    # Create NPCs
+    for k, v in npcObjectData.items():
+        gameObjects[k] = Npc(v)
+        
     return gameObjects
 
 """
@@ -89,69 +92,56 @@ DEFAULT OBJECT DATA
 """
 
 itemObjectData = {
-    "bed": Item(
-        "bed",
-        "It is comfortable and warm. No place for an Adventurer like yourself!",
-        "The sweet smell of fresh linen and days wasted.",
-        True,
-        False,
-        True,
-        True,
-        False,
-        False,
-        "I guess we could just forget this whole adventure thing that happens to be *your calling in life*. Or you could get up, you lazy, son of a bar wench",
-        False
-    ),
-    "mirror": Item(
-        "mirror",
-        "You can't help but wonder if the chain-mail makes your butt look big.",
-        False,
-        True,
-        False,
-        True,
-        True,
-        False,
-        False,
-        "You can't help but wonder if the chain-mail makes your butt look big.",
-        False
-    )
+    "bed": {
+        "name": "Bed",
+        "description": "It is comfortable and warm. No place for an Adventurer like yourself!",
+        "odour": "The sweet smell of fresh linen and days wasted.",
+        "isVisible": True,
+        "isTakeable": False,
+        "isUseable": True,
+        "isReusable": True,
+        "isUsed": False,
+        "useResp": "I guess we could just forget this whole adventure thing that happens to be *your calling in life*. Or you could get up, you lazy, son of a bar wench",
+    },
+    "mirror": {
+        "name": "Mirror",
+        "description": "You can't help but wonder if the chain-mail makes your butt look big.",
+        "isVisible": True,
+        "isTakeable": False,
+        "isUseable": True,
+        "isReusable": True,
+        "isUsed": False,
+        "useResp": "You can't help but wonder if the chain-mail makes your butt look big.",
+    }
 }
 
 containerObjectData = {
-    "wardrobe": Container(
-        "wardrobe",
-        "A large mahogany wardrobe.",
-        "This smells like grandma: mothballs and disappointment.",
-        True,
-        False,
-        False,
-        False,
-        False,
-        False,
-        False,
-        False,
-        True,
-        False,
-        True,
-        ["armour"]
-    ),
-    "chamber pot": Container(
-        "chamber pot",
-        "Why would you want to look at that? Gross.",
-        "Urrgh. Why would you do that? You know what goes in there!",
-        True,
-        True,
-        True,
-        True,
-        False,
-        "Why? I guess you could gross out your enemies with this.",
-        "You feel ... relieved.",
-        False,
-        True,
-        False,
-        True,
-        []
-    )
+    "wardrobe": {
+        "name": "Wardrobe",
+        "description": "A large mahogany wardrobe.",
+        "odour": "This smells like grandma: mothballs and disappointment.",
+        "isVisible": True,
+        "isContainer": True,
+        "isOpen": False,
+        "isCloseable": True,
+        "contents": ["armour"]
+    },
+    "chamber pot": {
+        "name": "Chamber Pot",
+        "description": "Why would you want to look at that? Gross.",
+        "odour": "Urrgh. Why would you do that? You know what goes in there!",
+        "isVisible": True,
+        "isTakeable": True,
+        "isUseable": True,
+        "isReusable": True,
+        "isUsed": False,
+        "takeResp": "Why? I guess you could gross out your enemies with this.",
+        "useResp": "You feel ... relieved.",
+        "isContainer": True,
+        "isOpen": False,
+        "isCloseable": True,
+        "contents": []
+    }
 }
 
 npcObjectData = {
